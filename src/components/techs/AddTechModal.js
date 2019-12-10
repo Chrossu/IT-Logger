ff0b51362825ee9bca6ee4aa2071718888705277
Chrossu <Chrossu@users.noreply.github.com>
2019-12-10 19:05:14 -0300

    Add technician and delete funcionality added

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { addTech } from '../../actions/techActions';
+import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js'
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
 
@@ -9,7 +12,13 @@ const AddTechModal = () => {
         if (firstName === '' || lastName === '') {
             M.toast({ html: 'Please, enter the First and Last name', classes: 'rounded' });
         } else {
-            console.log(firstName, lastName);
+            const newTech = {
+                firstName,
+                lastName
+            };
+
+            addTech(newTech);
+            M.toast({ html: `${firstName} ${lastName} was added as a new Tech`, classes: 'rounded'});
         }
 
         // Clear form
@@ -63,4 +72,8 @@ const AddTechModal = () => {
     )
 }
 
-export default AddTechModal
+AddTechModal.propTypes = {
+    addTech: PropTypes.func.isRequired
+}
+
+export default connect(null, { addTech })(AddTechModal);
--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { connect } from 'react-redux';
+import { deleteTech } from '../../actions/techActions'
+import PropTypes from 'prop-types';
+import M from 'materialize-css/dist/js/materialize.min.js'
 
-const TechItem = ({ tech }) => {
+const TechItem = ({ tech, deleteTech }) => {
+    const onDelete = () => {
+        const deletedTech = `${tech.firstName} ${tech.lastName}`;
+        deleteTech(tech.id)
+        M.toast({ html: `Tech ${deletedTech} was successfully deleted` });
+    }
     return (
         <li className="collection-item">
             <div>
                 {tech.firstName} {tech.lastName}
-                <a href="#!" className="secondary-content"><i className="material-icons grey-text">delete</i></a>
+                <a href="#!" className="secondary-content" onClick={onDelete}>
+                    <i className="material-icons grey-text">delete</i>
+                </a>
             </div>
         </li>
     )
@@ -16,4 +26,4 @@ TechItem.propTypes = {
     tech: PropTypes.object.isRequired
 }
 
-export default TechItem
+export default connect(null, { deleteTech })(TechItem);
